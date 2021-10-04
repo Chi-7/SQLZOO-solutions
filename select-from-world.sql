7b6cd3572cb79f834f0397015d23ade7b5e47227
Chi Chen <91863006+Chi-7@users.noreply.github.com>
2021-10-04 18:11:46 +0900

    Update select-from-world.sql

--- select-from-world.sql
+++ select-from-world.sql
@@ -6,7 +6,17 @@ SELECT from WORLD Tutoraial
 /*
 Show name, continent and population of all countries.
 */
-
 SELECT name, continent, population 
 FROM world
 
+-----#2
+/*
+Show name for the contries that have a population of at least 200 milion.
+1 milion = 1000000
+*/
+SELECT name 
+FROM world
+WHERE population > 200000000
+
+-----#3
+*/
