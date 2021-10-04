02141255af9dcc7dd6e962064d6a9a3a82bc0174
Chi Chen <91863006+Chi-7@users.noreply.github.com>
2021-10-04 18:20:21 +0900

    Update select-from-world.sql

--- select-from-world.sql
+++ select-from-world.sql
@@ -11,7 +11,7 @@ FROM world
 
 -----#2
 /*
-Show name for the contries that have a population of at least 200 milion.
+Show the name for the contries that have a population of at least 200 milion.
 1 milion = 1000000
 */
 SELECT name 
@@ -19,4 +19,28 @@ FROM world
 WHERE population > 200000000
 
 -----#3
+/*
+Show the name and per capita GDP for those countries with a population of at least 200 milion.
+Per capita GDP = gdp / population
 */
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
+
+-----#4
+/*
+Show the name and population in milions for the countries of the continent 'South America'/
+*/
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
+-----#5
+/*
+Show the name and population for France, Germany and Italy.
+*/
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+
