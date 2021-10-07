5379e128e5331cac200423d55203eee1bbd3d2b5
Chi Chen <91863006+Chi-7@users.noreply.github.com>
2021-10-07 12:28:54 +0900

    Update select-basics.sql

--- select-basics.sql
+++ select-basics.sql
@@ -1,30 +1,19 @@
-/*
-SECTION - SELECT basics
-*/
+--SECTION - SELECT basics
 
------#1
-/*
-The example shows the population of 'France'. The string shoukd be in 'single quotes'.
-Show the population of Germany.
-*/
+--#1 The example shows the population of 'France'. The string shoukd be in 'single quotes'. Show the population of Germany.
+
 SELECT population 
 FROM world
 WHERE name = 'Germany'
 
------#2
-/*
-The example shows name and population of more than one country by using IN clause.
-Show the name and population for 'Sweden', 'Norway' and 'Denmark'.
-*/
+--#2 The example shows name and population of more than one country by using IN clause. Show the name and population for 'Sweden', 'Norway' and 'Denmark'.
+
 SELECT name, population 
 FROM world
 WHERE name IN ('Sweden', 'Norway' , 'Denmark')
 
------#3
-/*
-The query shows name and area size between 250000 and 300000sq.km by using BETWEEN clause.
-show the name and are area size between 200000 and 250000
-*/
+--#3 The query shows name and area size between 250000 and 300000sq.km by using BETWEEN clause. show the name and are area size between 200000 and 250000
+
 SELECT name, area 
 FROM world
 WHERE area BETWEEN 200000 AND 250000
