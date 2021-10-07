62d266b4df7293b2207cb4df368ea7b26f9ce137
Chi Chen <91863006+Chi-7@users.noreply.github.com>
2021-10-07 11:44:04 +0900

    Update select-names.sql

--- select-names.sql
+++ select-names.sql
@@ -4,9 +4,102 @@ SELECT names
 
 -----#1
 /*
-Show countreis that start with Y
+Show the countreis that start with Y
 */
 SELECT name 
 FROM world
 WHERE name LIKE 'Y%'
 
+-----#2
+/*
+Show the countreis that end with Y
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%Y'
+
+-----#3
+/*
+Show the countreis that contain the letter x
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%x%'
+
+-----#4
+/*
+Show the countreis that end with land
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%land'
+
+-----#5
+/*
+Show the countreis that start with C and end with ia
+*/
+SELECT name 
+FROM world
+WHERE name LIKE 'C%ia'
+
+-----#6
+/*
+Show the countries contain oo in the name
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%oo%'
+
+-----#7
+/*
+Show the countries contain three or more a in the name
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%a%a%a%'
+
+-----#8
+/*
+Show the countries that have 't' as the second character
+Use '_' whitch means one letter
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '_t%'
+
+-----#9
+/*
+Show countries with two 'o' characters seperated by two others
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '%o__o%'
+
+-----#10
+/*
+Show countries that have exactly four charaters
+*/
+SELECT name 
+FROM world
+WHERE name LIKE '____'
+
+-----#11
+/*
+Show the countries that name are as same as the capital city
+*/
+SELECT name
+FROM world
+WHERE name = capital
+
+-----#12
+/*
+Show the countries that the capital is the country plus 'City'
+*/
+SELECT name
+FROM world
+WHERE capital LIKE '%City'
+
+-----#13
+/*
+Show the countries and the capital that capital name including country name
+*/
